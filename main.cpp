b84c10f97985a2f5a3574d1c51a31a1e8eedc1ad
jedibobo <44748473+jedibobo@users.noreply.github.com>
2020-05-16 16:27:15 +0800

    改善文件读取提示和界面清除
    改善文件读取提示和界面清除，同时利用已经写好的clear添加了停车场全部清空的选项。

--- main.cpp
+++ main.cpp
@@ -26,19 +26,24 @@ int main()
     cout << "输入其他任意键退出系统" << endl;
     int choice; cin >> choice;
     
-    switch (choice) {
+    switch (choice) 
+    {
     case 1:
+    {
         system("cls");
         p.Print_park();
         goto L1;
-        
+    }
     case 2:
+    {
         system("cls");
         p1.getstate();
         p1.Print_pavement();
         goto L1;
+    }
        
     case 3:
+    {
         cout << "0是手动输入，1是从文件中读取" << endl;
         cin >> flag;
         if (!flag)
@@ -71,27 +76,25 @@ int main()
             file.open(strFileName.c_str(), std::ios::in);           //【2】以读文件的方式打开文件
             if (!file.is_open())
             {
-                std::cout << "【NOTICE】The file is loaded unsuccessfully!" << std::endl;
+                std::cout << "【NOTICE】文件读取失败" << std::endl;
                 std::system("pause");
             }
             else
             {
-                std::cout << "【NOTICE】The file is loaded successfully and the file is being read........" << std::endl;
-                std::cout << "【NOTICE】Press any key to continue,please!" << std::endl;
+                std::cout << "【NOTICE】文件读取成功" << std::endl;
                 std::system("pause");
 
             }
+            system("cls");
             string strTemp;
             vector<string> strLinesBuffer;          //【1】以行的形式存储CSV文件读入的内容
             vector<string> strAll;                       //【2】以单个字段的形式存储CSV文件中所有的内容
             vector<std::vector<string>> strTable;                //【3】存储表格中的内容
-            //int iRows = 0;                                        //【1】CSV文件中的行数
-            //int iCols = 0;                                        //【2】CSV文件中的列数
             string line;
-            int num_cars=0;
-            while (getline(file, strTemp))
+            int num_cars = 0;
+            while (1)
             {
-                std::cout << "input the num of cars you want to push in, 0 to quit" << std::endl;
+                std::cout << "输入想要读取的车辆个数，输入0退出" << std::endl;
                 std::cin >> num_cars;
                 read_line += num_cars;
                 if (num_cars == 0) { system("cls"); goto L1; }
@@ -100,7 +103,7 @@ int main()
                 char* tmpString;
                 tmpString = strtok((char*)strTemp.c_str(), ";");
                 int count = 0;
-                while (getline(file, line)&&count<num_cars)   //整行读取，换行符“\n”区分，遇到文件尾标志eof终止读取
+                while (getline(file, line) && count < num_cars)   //整行读取，换行符“\n”区分，遇到文件尾标志eof终止读取
                 {
                     istringstream sin(line); //将整行字符串line读入到字符串流istringstream中
                     vector<string> fields; //声明一个字符串向量
@@ -108,12 +111,12 @@ int main()
                     while (getline(sin, field, ',')) //将字符串流sin中的字符读入到field字符串中，以逗号为分隔符
                     {
                         fields.push_back(field); //将刚刚读取的字符串添加到向量fields中
-                       
+
                     }
-                    string car_label =fields[0]; //清除掉向量fields中第一个元素的无效字符，并赋值给变量name
-               
-                    std::cout << car_label << std::endl; 
-                    
+                    string car_label = fields[0]; //清除掉向量fields中第一个元素的无效字符，并赋值给变量name
+
+                    std::cout << car_label << std::endl;
+
                     count += 1;
                     c.Plate = car_label;
                     check = p.Push(c);
@@ -134,19 +137,32 @@ int main()
                 }
 
             }
-
+            std::cout << "【NOTICE】已按照要求的数量读取并写入。" << std::endl;
         }
+    }
     case 4:
-        cout << "请输入车牌号" << endl;
-        cin >> co.Plate;
-        p.Pop1(co);
-        if(~p1.IsEmpty())
+    {
+        cout << "请输入车牌号,all全部清空" << endl;
+        string out;//车辆出的车牌号或者全部清空标识符
+        cin >> out;
+        if (out == "all")
         {
-            p1.Pop(ca);
-            p.Push(ca);
+            p.Clear();
+            goto L1;
         }
-        goto L1;
-        /**/
+        else
+        {
+            cin >> co.Plate;
+            p.Pop1(co);
+            if (!p1.IsEmpty())
+            {
+                p1.Pop(ca);
+                p.Push(ca);
+            }
+            goto L1;
+            /**/
+        }
+    }
     default:
         return 0;
     }
